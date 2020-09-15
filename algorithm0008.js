a7d2350e56f34fe45c186d248f8ed54a8eda2f8f
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-09-15 19:48:37 -0300

    add algorithm0008.js

---
+++ algorithm0008.js
@@ -0,0 +1,38 @@
+/**
+ * Difference
+ * Read four integer values named A, B, C and D. Calculate and print the difference of product A and B by the product of C and D (A * B - C * D).
+ * 
+ * Input
+ * The input file contains 4 integer values.
+ * 
+ * Output
+ * Print DIFERENCA (DIFFERENCE in Portuguese) with all the capital letters, according to the following example, with a blank space before and after the equal signal.
+ */
+
+var readline = require('readline')
+
+var interface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+var A = Number()
+var B = Number()
+var C = Number()
+var D = Number()
+var result = Number()
+
+interface.question('Insira A: ', numA => {
+  A = Number(numA)
+  interface.question('Insira B: ', numB => {
+    B = Number(numB)
+    interface.question('Insira C: ', numC => {
+      C = Number(numC)
+      interface.question('Insira D: ', numD => {
+        D = Number(numD)
+        result = ((A*B) + (C*D))
+        interface.write(`Product = ${result}`)
+      })
+    })
+  })
+})
