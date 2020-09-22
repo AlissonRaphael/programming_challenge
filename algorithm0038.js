2e3800cd10b15472b00750a4493f3afefe0715ad
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-09-22 12:09:53 -0300

    add algorithm0038.js

---
+++ algorithm0038.js
@@ -0,0 +1,29 @@
+/**
+ * Month
+ * Read an integer number between 1 and 12, including. Corresponding to this number, you must print the month of the year, in english, with the first letter in uppercase.
+ * 
+ * Input
+ * The input contains only an integer number.
+ * 
+ * Output
+ * Print the name of the month according to the input number, with the first letter in uppercase.
+ */
+
+var readline = require('readline')
+
+var interface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+var mounths = [
+  'January','February','March','April','May','June','July',
+  'August','September','October','November','December'
+]
+
+interface.question('Número do mês: ', num => {
+  var number = Number(num)
+
+  interface.write(`${mounths[number-1]}`)
+  interface.close()
+})
