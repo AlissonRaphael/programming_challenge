6a15e5d9760404ed935275ee26ccf7c855b2295f
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-09-23 13:34:57 -0300

    refact algorithm0043.js

--- algorithm0043.js
+++ algorithm0043.js
@@ -17,26 +17,19 @@ var interface = readline.createInterface({
   output: process.stdout
 })
 
-var numbers = []
+var number = Number()
 var pairs = Number()
 
+async function init(){
+  for(var i = 1; i <= 5; i++){
+    number = Number(await new Promise(resolve => {
+      interface.question(`Número ${i}: `, input => resolve(input))
+    }))
+    if(number % 2 == 0) pairs += 1
+  }
+  
+  interface.write(`${pairs} pares.`)
+  interface.close()
+}
 
-interface.question(': ', num1 => {
-  numbers.push(Number(num1))
-  interface.question(': ', num2 => {
-    numbers.push(Number(num2))
-    interface.question(': ', num3 => {
-      numbers.push(Number(num3))
-      interface.question(': ', num4 => {
-        numbers.push(Number(num4))
-        interface.question(': ', num5 => {
-          numbers.push(Number(num5))
-
-          numbers.forEach(number => {if(number % 2 == 0) pairs += 1})
-          interface.write(`${pairs} pares.`)
-          interface.close()
-        })
-      })
-    })
-  })
-})
+init()
