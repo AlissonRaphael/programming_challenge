4de25d604017c80cd185bf535921d6ca84e2cd14
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-09-26 17:29:52 -0300

    add algorithm0057

---
+++ beginner/algorithm0057.js
@@ -0,0 +1,16 @@
+/**
+ * Sequence IJ 2
+ * Make a program that prints the sequence like the following exemple.
+ * 
+ * Input
+ * This problem doesn't have input.
+ * 
+ * Output
+ * Print the sequence like the example below.
+ */
+
+for(var i = 1; i <= 9; i++){
+  for(var j = 7; j >= 5; j--){
+    console.log(`I = ${i}, J = ${j}`)
+  }
+}
