e1e338cd8a248d88a72ffe751cd12742e8b61374
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-09-27 16:23:09 -0300

    add algorithm0064

--- beginner/algorithm0064.js
+++ beginner/algorithm0064.js
@@ -9,3 +9,31 @@
  * For each test case, print the corresponding quadrant which these coordinates belong, as in the example.
  */
 
+var readline = require('readline')
+
+var interface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+async function init(){
+  while(true){
+    var coordinates = await new Promise(resolve => {
+      interface.question(`Teste: `, input => resolve(input.split(' ')))
+    })
+    var x = Number(coordinates[0])
+    var y = Number(coordinates[1])
+
+    if(coordinates[0] == '' || coordinates[1] == '') break
+    if(x==0 || y==0) break
+
+    if(x>0 && y>0) interface.write('Primeiro\n')
+    if(x<0 && y>0) interface.write('Segundo\n')
+    if(x<0 && y<0) interface.write('Terceiro\n')
+    if(x>0 && y<0) interface.write('Quarto\n')
+  }
+
+  interface.close()
+}
+
+init()
