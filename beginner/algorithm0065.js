33fafcc901f44b34e906000be782044920149365
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-09-27 17:21:47 -0300

    add algorithm0065

--- beginner/algorithm0065.js
+++ beginner/algorithm0065.js
@@ -11,3 +11,34 @@
  * Obs.: Be carefull. The division between two integers in some languages generates another integer. Use cast:)
  */
 
+var readline = require('readline')
+
+var interface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+async function init(){
+  var quantity = await new Promise(resolve => {
+    interface.question(`Quantidade de testes: `, input => resolve(input.split(' ')))
+  })
+
+  for(var i = 1; i <= quantity; i++){
+    var numbers = await new Promise(resolve => {
+      interface.question(`Teste ${i}: `, input => resolve(input.split(' ')))
+    })
+    var x = Number(numbers[0])
+    var y = Number(numbers[1])
+
+    if(x==0) i += quantity
+
+    if(y==0) interface.write(`0.0`)
+    if(y>0) interface.write(`${(x/y).toFixed(1)}`)
+    if(y<0) interface.write('Divisão impossivel')
+
+  }
+
+  interface.close()
+}
+
+init()
