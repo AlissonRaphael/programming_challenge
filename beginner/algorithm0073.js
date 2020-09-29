4c07f64c87e93c87fbd8e0f4ebcd5f8b3f170245
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-09-29 18:38:11 -0300

    init algorithm0073

---
+++ beginner/algorithm0073.js
@@ -0,0 +1,10 @@
+/**
+ * Squared and Cubic
+ * Write a program that reads an integer N (1 < N < 1000). This N is the number of output lines printed by this program.
+ * 
+ * Input
+ * The input file contains an integer N.
+ * 
+ * Output
+ * Print the output according to the given example.
+ */
