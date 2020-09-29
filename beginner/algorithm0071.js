24214079b1900d87d273c837c392ea1d5b362ce5
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-09-29 18:24:41 -0300

    add algorithm0071

--- beginner/algorithm0071.js
+++ beginner/algorithm0071.js
@@ -8,3 +8,39 @@
  * Output
  * It should be written the message: "MUITO OBRIGADO" and the amount of customers who fueled each fuel type, as an example.
  */
+
+var readline = require('readline')
+
+var interface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+async function init(){
+  var fuelType = Number()
+  var alcool = Number()
+  var gasolina = Number()
+  var diesel = Number()
+
+  while(true){
+    fuelType = await new Promise(resolve => {
+      interface.question(
+        `Tipo de combustivel ['1-Álcool','2-Gasolina','3-Diesel','4-Sair']: \n>>> `,
+        input => resolve(Number(input))
+      )
+    })
+
+    if(fuelType == 4) break
+    if(fuelType < 1 || fuelType > 4) interface.write('Insira um tipo válido!\n')
+    if(fuelType == 1) alcool += 1
+    if(fuelType == 2) gasolina += 1
+    if(fuelType == 3) diesel += 1
+  }
+
+  interface.write(`Alcool: ${alcool}\n`)
+  interface.write(`Gasolina: ${gasolina}\n`)
+  interface.write(`Diesel: ${diesel}\n`)
+  interface.close()
+}
+
+init()
