a215c83c746278562fd58aedcdf55cebce8547be
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-09-29 17:49:12 -0300

    add algorithm0070

--- beginner/algorithm0070.js
+++ beginner/algorithm0070.js
@@ -8,3 +8,37 @@
  * Output
  * Print all numbers according to above description, always in ascending order.
  */
+
+var readline = require('readline')
+
+var interface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+async function init(){
+  var num1 = await new Promise(resolve => {
+    interface.question(`Insira o número I: `, input => resolve(Number(input)))
+  })
+
+  var num2 = await new Promise(resolve => {
+    interface.question(`Insira o número II: `, input => resolve(Number(input)))
+  })
+
+  if(num1 < num2){
+    for(var i = num1; i <= num2; i++){
+      if(i%5 == 2) interface.write(`${i}\n`)
+      if(i%5 == 3) interface.write(`${i}\n`)
+    }
+  }
+  if(num1 > num2){
+    for(var i = num2; i <= num1; i++){
+      if(i%5 == 2) interface.write(`${i}\n`)
+      if(i%5 == 3) interface.write(`${i}\n`)
+    }
+  }
+
+  interface.close()
+}
+
+init()
