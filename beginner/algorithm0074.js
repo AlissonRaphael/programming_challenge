883d5f65488fafa2a8fbe335adc412411b181c37
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-09-30 17:07:20 -0300

    add algorithm0074

--- beginner/algorithm0074.js
+++ beginner/algorithm0074.js
@@ -9,3 +9,29 @@
  * Print the output according to the given example.
  */
 
+var readline = require('readline')
+
+var interface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+async function init(){
+  var num = await new Promise(resolve => {
+    interface.question(`Insira o número: `, input => resolve(Number(input)))
+  })
+
+  for(var i = 1; i <= num*2; i++){
+    
+    for(var j = 1; j <= 2; j++){
+      interface.write(`${i} `)
+      if(j == 1) interface.write(`${i**2} ${i*(i**2)}\n`)
+      if(j == 2) interface.write(`${(i**2)+1} ${(i*(i**2))+1}\n`)
+    }
+
+  }
+  
+  interface.close()
+}
+
+init()
