b46ffec87ffec68af7a19c08cd01bdcb90360783
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-09-30 17:45:11 -0300

    add algorithm0075

--- beginner/algorithm0075.js
+++ beginner/algorithm0075.js
@@ -8,3 +8,28 @@
  * Output
  * Each sequence must be printed in one line, with a blank space between each number, like the following example.
  */
+
+var readline = require('readline')
+
+var interface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+async function init(){
+  var numbers = await new Promise(resolve => {
+    interface.question(`Insira o número: `, input => resolve(input.split(' ')))
+  })
+  numbers = numbers.map(num => Number(num))
+
+  for(var i = 1; i <= numbers[1]; i += numbers[0]){
+    for(var j = 0; j <= numbers[0]-1; j++){
+      if(i+j > numbers[1]) break
+      interface.write(`${i+j} `)
+    }
+    interface.write('\n')
+  }
+  interface.close()
+}
+
+init()
