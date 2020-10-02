c67924672ff3818c53d408464c78d90b0d45b096
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-02 18:22:32 -0300

    refact algorithm0079

--- beginner/algorithm0079.js
+++ beginner/algorithm0079.js
@@ -21,14 +21,35 @@ async function init(){
     interface.question('Insira a quantidade de números: ', input => resolve(Number(input)))
   })
 
-  var sequence = []
-  for(var i = 2; i < sequenceQuantity; i++){
-    if(i === 0 || i === 1) sequence.push(i)
-    if(i > 1) sequence.push(sequence[sequence.length-2]+sequence[sequence.length-1])
+  var n2Enesima = 0
+  var n1Enesima = 1
+  var n = 0
+  var counter = 0
+  while(counter < sequenceQuantity){
+    if(counter == 0) interface.write(`${counter}`)
+    if(counter == 1) interface.write(`, ${counter}`)
+
+    if(counter > 1){
+      n = n2Enesima + n1Enesima
+      interface.write(`, ${n}`)
+      n2Enesima = n1Enesima
+      n1Enesima = n
+    }
+
+    counter += 1
   }
 
-  interface.write(`${sequence.toString()}`)
-  interface.close()
+  /**
+   * Array Solution
+   * var sequence = []
+   * for(var i = 2; i < sequenceQuantity; i++){
+   *   if(i === 0 || i === 1) sequence.push(i)
+   *   if(i > 1) sequence.push(sequence[sequence.length-2]+sequence[sequence.length-1])
+   * }
+   * interface.write(`${sequence.toString()}`)
+   */
+
+   interface.close()
 }
 
 init()
