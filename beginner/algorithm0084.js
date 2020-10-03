1af6e5d59144f99ebd686d3d6c36112b99c5ebda
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-03 17:16:51 -0300

    add algorithm0084

--- beginner/algorithm0084.js
+++ beginner/algorithm0084.js
@@ -9,3 +9,22 @@
  * Write all positive divisors of N, one value per line.
  */
 
+var readline = require('readline')
+
+var interface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+async function init(){
+  var num = await new Promise(resolve => {
+    interface.question(': ', input => resolve(Number(input)))
+  })
+
+  for(var i = 0; i <= num; i++){
+    if(num%i === 0) interface.write(`${i} `)
+  }
+  interface.close()
+}
+
+init()
