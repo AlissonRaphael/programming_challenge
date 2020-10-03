747841f42960d5dcde1432d1744438a6f82da753
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-03 17:40:43 -0300

    add algorithm0085

--- beginner/algorithm0085.js
+++ beginner/algorithm0085.js
@@ -11,3 +11,36 @@
  * Print the sum of all consecutive odd numbers from X.
  */
 
+var readline = require('readline')
+
+var interface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+async function init(){
+  var numberOfTests = await new Promise(resolve => {
+    interface.question('Número de testes: ', input => resolve(Number(input)))
+  })
+
+  var tests = []
+  for(var i = 0; i < numberOfTests; i++){
+    var test = await new Promise(resolve => {
+      interface.question(`Teste ${i+1}: `, input => resolve(input.split(" ")))
+    })
+    test = test.map(num => Number(num))
+    tests.push(test)
+  }
+
+  tests.map(test => {
+    var sum = 0
+    for(var i = test[0]; i < (test[0]+test[1]); i++){
+      if(i%2 != 0) sum += i
+    }
+    interface.write(`${sum}\n`)
+  })
+
+  interface.close()
+}
+
+init()
