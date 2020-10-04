057a29eaf2500a62bd1cfdd276f0b5a866d26d3f
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-04 14:08:32 -0300

    add algorithm0092

--- beginner/algorithm0092.js
+++ beginner/algorithm0092.js
@@ -9,3 +9,25 @@
  * For each number of the array that is equal to 10 or less, print "A [i] = x", where i is the position of the array and x is the number stored in the position, with one digit after the decimal point.
  */
 
+var readline = require('readline')
+
+var interface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+async function init(){
+  var array = []
+  for(var i = 0; i < 10; i++){
+    array.push(await new Promise(resolve => {
+      interface.question(`Número ${i+1}: `, input => resolve(Number(input)))
+    }))
+  }
+
+  array.map((num, index) => {
+    if(num <= 10) interface.write(`X[${index}] = ${num}\n`)
+  })
+  interface.close()
+}
+
+init()
