b3a1d2e9ae3b2c0cf7c8cdb139afa100b30e8bd5
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-05 18:25:54 -0300

    add algorithm0094

--- beginner/algorithm0094.js
+++ beginner/algorithm0094.js
@@ -9,3 +9,31 @@
  * For each test case in the input, print the message "Fib(N) = X", where X is the N-th term of the Fibonacci series.
  */
 
+var readline = require('readline')
+
+var interface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+async function init(){
+  var testQuantity = await new Promise(resolve => {
+    interface.question('Insira a quantidade de testes: ', input => resolve(Number(input)))
+  })
+  
+  for(var j = 0; j < testQuantity; j++){
+    var term = await new Promise(resolve => {
+      interface.question('Insira o termo: ', input => resolve(Number(input)))
+    })
+
+    var sequence = []
+    for(var i = 0; i <= term; i++){
+      if(i === 0 || i === 1) sequence.push(i)
+      if(i > 1) sequence.push(sequence[sequence.length-2]+sequence[sequence.length-1])
+    }
+    interface.write(`Fib(${term}) = ${sequence[term]}\n`)
+  }
+  interface.close()
+}
+
+init()
