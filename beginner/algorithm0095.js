1efe88c258b843ea842c87e0738d852846a38832
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-05 18:40:16 -0300

    add algorithm0095

--- beginner/algorithm0095.js
+++ beginner/algorithm0095.js
@@ -9,3 +9,32 @@
  * For each position of the array N, print "N[i] = x", where i is the array position and x is the number stored in that position.
  */
 
+var readline = require('readline')
+
+var interface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+async function init(){
+  var num = await new Promise(resolve => {
+    interface.question('Valor de T (2 ≤ T ≤ 50): ', input => resolve(Number(input)))
+  })
+  
+  var arr = []
+  for(var i = 0; i < 1000; i++){
+
+    for(var j = 0; j < num; j++){
+      if(arr.length <= 1000) arr.push(j)
+      if(arr.length === 1000) break
+    }
+
+    if(arr.length === 1000) break
+
+  }
+  
+  arr.forEach((num, index) => interface.write(`N(${index}) = ${num}\n`))
+  interface.close()
+}
+
+init()
