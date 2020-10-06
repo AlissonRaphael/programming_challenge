e76a720db3df8c27723772e2a1e61300cf8deee6
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-06 17:20:48 -0300

    add algorithm0098

--- beginner/algorithm0098.js
+++ beginner/algorithm0098.js
@@ -9,3 +9,41 @@
  * The first line displays the message “Menor valor:” followed by a space and the lowest number read in the input. The second line displays the message “Posicao:” followed by a space and the array position in which the lowest number is, remembering that the array starts at the zero position.
  */
 
+var readline = require('readline')
+
+var interface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+async function init(){
+  var size = await new Promise(resolve => {
+    interface.question('Tamanho do vetor: ', input => resolve(Number(input)))
+  })
+  
+  var arr = []
+  var value = Number()
+  var index = Number()
+
+  for(var i = 0; i < size; i++){
+    var newNum = await new Promise(resolve => {
+      interface.question('Valor: ', input => resolve(Number(input)))
+    })
+
+    if(i === 0){
+      value = newNum
+      index = i
+    }
+
+    if(newNum < value){
+      value = newNum
+      index = i
+    }
+  }
+  
+  interface.write(`Menor valor: ${value}\n`)
+  interface.write(`Posição: ${index}\n`)
+  interface.close()
+}
+
+init()
