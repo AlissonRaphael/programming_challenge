e98ac0d636c0e36a18b95e9f6c936986d2242787
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-08 17:00:50 -0300

    add algorithm0106

--- beginner/algorithm0106.js
+++ beginner/algorithm0106.js
@@ -11,3 +11,46 @@
  * Print the calculated result (sum or average), with one digit after the decimal point.
  */
 
+
+var readline = require('readline')
+
+var interface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+async function init(){
+  var operation = await new Promise(resolve => {
+    interface.question(
+      'Operação a ser realizada [S-Soma, M-Média]: ',
+      input => resolve(input.toUpperCase())
+    )
+  })
+
+  var sum = 0
+  var matrix = []
+  for(var i = 0; i < 12; i++){
+    var line = []
+
+    for(var j = 0; j < 12; j++){
+      var num = await new Promise(resolve => {
+        interface.question(
+          `Valor da Linha[${i}] Coluna[${j}]: `,
+          input => resolve(Number(input))
+        )
+      })
+
+      line.push(num)
+      if(j > i && i < 11-j) sum += num
+    }
+
+    matrix.push(line)
+  }
+
+  console.table(matrix)
+  if(operation === 'S') interface.write(`Soma: ${sum}`)
+  if(operation === 'M') interface.write(`Média: ${sum/72}`)
+  interface.close()
+}
+
+init()
