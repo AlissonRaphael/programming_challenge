deb61ce0715c015fc1b4b3ec34139d1e3b658290
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-09 18:34:05 -0300

    add algorithm0110

--- beginner/algorithm0110.js
+++ beginner/algorithm0110.js
@@ -9,3 +9,39 @@
  * For each integer number of input, print the corresponding array according to the example. (the values ​​of the arrays must be formatted in a field of size 3 right justified and separated by a space. None space must be printed after the last character of each row of the array. A blank line must be printed after each array.
  */
 
+var readline = require('readline')
+
+var interface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+async function init(){
+  while(true){
+    var num = await new Promise(resolve => {
+      interface.question(`: `, input => resolve(Number(input)))
+    })
+    if(num === 0) break
+
+    var term = Number()
+    var matrix = []
+    for(var i = 1; i <= num; i++){
+      var line = []
+  
+      for(var j = 1; j <= num; j++){
+        term = i
+        if(j < term) term = j
+        if (num - i + 1 < term) term = num - i + 1;
+        if (num - j + 1 < term) term = num - j + 1;
+  
+        line.push(term)
+      }
+      matrix.push(line)
+    }
+  
+    console.table(matrix)
+  }
+  interface.close()
+}
+
+init()
